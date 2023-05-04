5205cf1d186e990d06cce257965e09f64884c5c0
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-05-04 04:09:56 -0600

    Add function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasks = [
+  { description: "Task 1", completed: false, index: 0 },
+  { description: "Task 2", completed: false, index: 1 },
+  { description: "Task 3", completed: true, index: 2 },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+function renderTasks() {
+  const todoList = document.getElementById('todo-list');
+  
+  tasks.sort((a, b) => a.index - b.index).forEach(task => {
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    const description = document.createTextNode(task.description);
+    
+    listItem.appendChild(checkbox);
+    listItem.appendChild(description);
+    todoList.appendChild(listItem);
+  });
 }
 
-document.body.appendChild(component());
+document.addEventListener("DOMContentLoaded", () => {
+  renderTasks();
+});
