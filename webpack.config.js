c4e860ab85cc7f4a0ed38b9fc12f5ef96256e648
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-05-06 06:34:13 -0600

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = async () => {
     output: {
       filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
     },
     devServer: {
       static: './dist',
